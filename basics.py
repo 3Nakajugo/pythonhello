9c1f27bc72996e5ee19b3118c2d0c9fea1589586
3Nakajugo <edinahmargarita@gmail.com>
2021-03-06 12:23:39 +0300

    add basic syntax

---
+++ basics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+"""print results"""
+print ("hello world") 
+
+""" Line continuation """
+sum = number_one + \
+      number_two +\
+      number_three
+
+#multiple code in single line
+import sys; x = 2; c = 4;
